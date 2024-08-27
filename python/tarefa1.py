90e2c52318073ca62abc2890f437feb06a576ade
felipeb <felipeborgaco@hotmail.com>
2024-08-27 15:44:17 -0300

    first commit

---
+++ python/tarefa1.py
@@ -0,0 +1,8 @@
+nome="felipe"
+idade=22
+cidade="imperatriz"
+
+def informacoes_pessoais(nome,idade,cidade):
+    return print(nome,idade,cidade)
+
+informacoes_pessoais(nome,idade,cidade)
---
+++ python/tarefa2.py
@@ -0,0 +1,6 @@
+contador=0
+def incrementar_contador(contador):
+    contador+=1
+    mensagem=""
+    return contador
+print(incrementar_contador(contador),mensagem) #mensagem nao  definida
